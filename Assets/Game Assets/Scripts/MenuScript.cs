ee44753d55d9d4abcc8bd14b25587182c56bdff5
Sam Nelson <snelson3@cs.uoregon.edu>
2016-05-09 01:47:53 -0800

    Added really simple scene

---
+++ Assets/Game Assets/Scripts/MenuScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+		}
+	}
+}
