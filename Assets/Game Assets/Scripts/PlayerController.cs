0d9b97eac906475115ae6457227c0042f5ab5fa4
bryonf <fleming_bryon@hotmail.com>
2016-05-12 16:06:14 -0700

    mountain/volcano terrain added, PlayerController fixed

--- Assets/Game Assets/Scripts/PlayerController.cs
+++ Assets/Game Assets/Scripts/PlayerController.cs
@@ -125,7 +125,7 @@ public class PlayerController : MonoBehaviour {
         {
             other.gameObject.SetActive(false);
             isPowered = true;
-			float scaleVal = 0.4f;
+			scaleVal = 0.4f;
             rb.transform.localScale = new Vector3(scaleVal, scaleVal, scaleVal);
             powerEnd = Time.time + powerTime;
         }
