620bb72bf20b55299e06e1d9cec3966e7b3bb53b
bryonf <fleming_bryon@hotmail.com>
2016-05-13 06:25:12 -0700

    volcano is now dynamically generated to prevent git/Unity issues. currently works for Windows, but may need alterations to work for Mac

---
+++ Assets/Game Assets/Scripts/GenerateVolcano.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateVolcano : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        TerrainData tdata = new TerrainData();
+		tdata.SetDetailResolution (1024, 16);
+		tdata.size = new Vector3 (31.25f, 600f, 31.25f);
+		tdata.thickness = 1;
+		tdata.heightmapResolution = 513;
+		SplatPrototype[] splats = new SplatPrototype[1];
+		splats [0] = new SplatPrototype ();
+		splats [0].texture = (Texture2D)Resources.Load ("VolcanoTextures/ground2_Specular");
+		tdata.splatPrototypes = splats;
+
+		int h = tdata.heightmapHeight;
+		int w = tdata.heightmapWidth;
+		float[,] data = new float[h, w];
+		using (System.IO.FileStream file = System.IO.File.OpenRead("Assets/Resources/TerrainHeightMaps/terrain_windows.raw"))
+		using (System.IO.BinaryReader reader = new System.IO.BinaryReader(file))
+		{
+			for (int y = 0; y < h; y++)
+			{
+				for (int x = 0; x < w; x++)
+				{
+					float v = (float)reader.ReadUInt16() / 0xFFFF;
+					data[y, x] = v;
+				}
+			}
+		}
+		tdata.SetHeights(0, 0, data);
+
+		Terrain.CreateTerrainGameObject(tdata).name = "Volcano";
+	}
+}
