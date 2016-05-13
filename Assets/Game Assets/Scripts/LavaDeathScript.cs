d6b3b8db0038a01b09facd3267890b6612c91cf2
bryonf <fleming_bryon@hotmail.com>
2016-05-12 20:03:33 -0700

    added death handling to lava (including simple sinking animation)

---
+++ Assets/Game Assets/Scripts/LavaDeathScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaDeathScript : MonoBehaviour {
+	
+	private bool killingPlayer;
+	
+	void Start() {
+		killingPlayer = false;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Player") && !killingPlayer) {
+			killingPlayer = true;
+			GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraController>().SetPlayer(null);
+			StartCoroutine(GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().LavaDeath());
+		}
+	}
+}
