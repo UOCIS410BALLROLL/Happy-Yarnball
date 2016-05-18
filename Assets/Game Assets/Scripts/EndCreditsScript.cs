911a1f725aeb465ab2e038c5a21a0ffa5e32cf6d
Anu Deodhar <adeodhar@uoregon.edu>
2016-05-18 11:38:28 -0700

    added ending credits scene

---
+++ Assets/Game Assets/Scripts/EndCreditsScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class EndCreditsScript : MonoBehaviour {
+
+	public GameObject camera;
+	public int speed = 1;
+	public string level;
+
+	void Update() {
+		camera.transform.Translate (Vector3.down * Time.deltaTime * speed);
+	}
+
+	IEnumerator waitFor() {
+		yield return new WaitForSeconds (20);
+		SceneManager.LoadScene (level);
+	}
+}
+
+
