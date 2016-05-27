2a1ff9307f8889bdc8419359879944d2fe588b81
bryonf <fleming_bryon@hotmail.com>
2016-05-26 22:34:37 -0700

    small animation fix

--- Assets/Game Assets/Scripts/LavaAnimation.cs
+++ Assets/Game Assets/Scripts/LavaAnimation.cs
@@ -10,16 +10,28 @@ public class LavaAnimation : MonoBehaviour {
 	private Rigidbody rb;
 	private float start_x, start_z;
 	private bool reverse;
+	private Vector2 direction;
 	
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 		start_x = rb.transform.position.x;
 		start_z = rb.transform.position.z;
 		reverse = false;
+		StartCoroutine (ManageMove ());
+	}
+
+	IEnumerator ManageMove() {
+		Vector2 newDirection;
+		float angle;
+		while (true) {
+			while ((angle = (Vector2.Angle ((newDirection = Random.insideUnitCircle * speed), direction) + 360) % 360) < 90 || angle > 270) {}
+			direction = newDirection;
+			yield return new WaitForSeconds (2.5f);
+		}
 	}
 	
 	void FixedUpdate() {
-		if(!reverse) {
+		/*if(!reverse) {
 			Vector3 endPosition = new Vector3(end_x, rb.transform.position.y, end_z);
 			rb.transform.position = Vector3.MoveTowards(rb.transform.position, endPosition, Time.deltaTime * speed);
 			
@@ -30,6 +42,9 @@ public class LavaAnimation : MonoBehaviour {
 			rb.transform.position = Vector3.MoveTowards(rb.transform.position, startPosition, Time.deltaTime * speed);
 			
 			reverse = !(rb.transform.position == startPosition);
-		}
+		}*/
+
+		gameObject.GetComponent<Renderer> ().material.SetTextureOffset ("_MainTex",
+			gameObject.GetComponent<Renderer>().material.GetTextureOffset("_MainTex") + direction * Time.deltaTime);
 	}
 }
