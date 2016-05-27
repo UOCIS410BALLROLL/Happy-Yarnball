54b1ecb1c781ea048ce119275ac8179b2767bdec
bryonf <fleming_bryon@hotmail.com>
2016-05-27 04:21:44 -0700

    particle emitter will conditionally destroy itself if running on web player for efficiency

---
+++ Assets/Game Assets/Scripts/ParticleDestroyer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleDestroyer : MonoBehaviour {
+
+	void Start () {
+		#if UNITY_WEBPLAYER
+		Destroy(gameObject);
+		#endif
+	}
+}
