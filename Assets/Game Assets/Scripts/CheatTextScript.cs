1dcab1ee450c92d3bc326a1c72edc1ace6ca66fc
Sam Nelson <snelson3@cs.uoregon.edu>
2016-05-27 20:03:14 -0800

    Main menu partially implemented

---
+++ Assets/Game Assets/Scripts/CheatTextScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CheatTextScript : MonoBehaviour {
+	public Text cheatText;
+
+	// Use this for initialization
+	void Start () {
+		if (PlayerPrefs.GetInt ("Cheatyface") == 0) {
+			cheatText.text = "";
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
